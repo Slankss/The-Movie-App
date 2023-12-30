6fb7e7005295e8c28f7253e31568a9f5aeb54ab6
okankkl <okankukull@gmail.com>
2023-12-30 15:57:35 +0300

    i made image slider in movie list page

--- app/src/main/java/com/okankkl/themovieapp/view/MovieListPage.kt
+++ app/src/main/java/com/okankkl/themovieapp/view/MovieListPage.kt
@@ -1,23 +1,38 @@
 package com.okankkl.themovieapp.view
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -28,8 +43,13 @@ import coil.compose.AsyncImage
 import com.okankkl.themovieapp.enum_sealed.Pages
 import com.okankkl.themovieapp.enum_sealed.Resources
 import com.okankkl.themovieapp.model.Movie
+import com.okankkl.themovieapp.ui.theme.BackgroundColor
+import com.okankkl.themovieapp.ui.theme.LightBlue
 import com.okankkl.themovieapp.util.Util
 import com.okankkl.themovieapp.viewModel.MovieListViewModel
+import androidx.compose.runtime.*
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import kotlinx.coroutines.delay
 
 @Composable
 fun MovieList(navController: NavController){
@@ -49,9 +69,12 @@ fun MovieList(navController: NavController){
         when(movieList.value){
             is Resources.Loading -> Loading()
             is Resources.Success -> {
+                TrendMovies(movies = (movieList.value as Resources.Success).data as List<Movie>)
+
                 PosterList(
                     movieList = (movieList.value as Resources.Success).data as List<Movie>,
                     navController)
+
             }
             is Resources.Failed -> {
                 Failed(
@@ -62,6 +85,100 @@ fun MovieList(navController: NavController){
 
 
     }
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TrendMovies(movies : List<Movie>){
+
+    var time by remember { mutableStateOf(0) }
+
+    var pageState = rememberPagerState(
+        pageCount = {
+            4
+        },
+        initialPage = 0,
+
+    )
+
+    LaunchedEffect(key1 = time, block = {
+        snapshotFlow { pageState.currentPage }.collect { page ->
+            time = 0
+        }
+    })
+
+
+    HorizontalPager(
+        state = pageState,
+        modifier = Modifier.fillMaxWidth()
+
+    )
+    { page ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp)
+                .height(200.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .shadow(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(12.dp)
+                ),
+        ){
+            AsyncImage(
+                model = Util.IMAGE_BASE_URL +movies[page].backdropPath,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .clickable {
+                        //onClick(movie.id)
+                    },
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = movies[page].title,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .background(
+                        color = Color(0x24FFFFFF)
+                    )
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                style = MaterialTheme.typography.headlineLarge.copy(
+
+                ),
+
+
+            )
+        }
+    }
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(bottom = 10.dp, top = 10.dp),
+        horizontalArrangement = Arrangement.Center
+    ){
+        repeat(pageState.pageCount) { iteration ->
+            val color = if(pageState.currentPage == iteration) LightBlue else Color.White
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(
+                        color = color
+                    )
+                    .size(8.dp)
+            )
+        }
+    }
+    Text(
+        text = time.toString()
+
+    )
 
 }
 
